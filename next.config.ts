210f9ae5545475df8ee9e07d1d692e29323d02ad
yuya.arashidani <arashidani.yuya@e-grid.co.jp>
2025-08-26 23:12:10 +0900

    fix: buildエラー修正

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 import createMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
 
 const withNextIntl = createNextIntlPlugin()
 const withMDX = createMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-  },
 })
 
 const nextConfig: NextConfig = {
